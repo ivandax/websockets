41a59515a9477a70d476d8be2b1829b415039577
ivandax <ivandax_89@hotmail.com>
2024-03-02 11:12:27 +0100

    Add typescript

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import typescript from 'rollup-plugin-typescript2';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), typescript()],
 });
