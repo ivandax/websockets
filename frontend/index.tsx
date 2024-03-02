e5cc53e52aed842e2f67959738631adbce6bf4d7
ivandax <ivandax_89@hotmail.com>
2024-03-02 12:27:40 +0100

    Add compiler errors checking with vite plugin checker

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/App";
 
-const toaster = document.getElementById("root");
+const rootElement = document.getElementById("root");
 
-const root = ReactDOM.createRoot(toaster as HTMLElement);
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
 root.render(<App />);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+    plugins: [react(), checker({ typescript: true })],
 });
