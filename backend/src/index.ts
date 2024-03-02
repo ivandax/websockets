ed112fb4cdb36a1fd44edf6751040af73bdfc5bb
ivandax <ivandax_89@hotmail.com>
2024-03-02 19:37:17 +0100

    Add support to support websockets

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import * as WebSocket from "ws";
+import * as http from "http";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const webSocketsServer = new WebSocket.Server({ server });
 
 app.use(cors({}));
 
@@ -10,6 +14,19 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello, world!" });
 });
 
-app.listen(PORT, () => {
+webSocketsServer.on("connection", (ws: WebSocket) => {
+  console.log("Client connected");
+
+  ws.on("message", (message: string) => {
+    console.log(`Received message: ${message}`);
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
